adb082660ddaaef524d2aaa2d743f2a71353554c
Hichem Fantar <hichemfantar2049@gmail.com>
2024-09-13 16:15:19 +0100

    Refactor main.tsx to use Dashboard component instead of App component

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,10 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import { Dashboard } from "./Dashboard.tsx";
 
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
-		<App />
+		<Dashboard />
 	</StrictMode>
 );
